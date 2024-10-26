5809f0033e93791b55ee244bbae4796c23ac5a87
Darien Moment <dmoment18@gmail.com>
2024-10-26 13:35:25 -0400

    Updated code w/ web files

--- src/App.js
+++ src/App.js
@@ -1,86 +1,15 @@
-import styled from 'styled-components';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
-import Section from './components/Section';
-import StaffCredits from './components/StaffCredits';
-import WelcomeHeader from './components/WelcomeHeader';
-import homecomingImage from './homecoming.jpg';
-import { device } from './device';
-
-const FirstFold = styled.div`
-  display: flex;
-  margin-top: -3rem;
-  flex-direction: column-reverse;
-
-  @media ${device.tablet} {
-    flex-direction: row;
-    align-items: flex-start;
-  }
-`;
-
-const MainImage = styled.iframe`
-  display: none;
-
-  @media ${device.tablet} {
-    display: block;
-    width: 60%;
-    height: 35rem;
-  }
-`;
-
-const MobileMainImage = styled.img`
-  width: 100%;
-  height: 20rem;
-  display: block;
-
-  @media ${device.tablet} {
-    display: none;
-  }
-`;
-
-const Ad = styled.img`
-  margin: 0 2rem;
-  display: none;
-
-  @media ${device.tablet} {
-    display: block;
-  }
-`;
-
-const MobileAd = styled.img`
-  margin: 0 2rem;
-  display: block;
-
-  @media ${device.tablet} {
-    display: none;
-  }
-`;
+import Home from './Home';
 
 function App() {
-  const sectionNames = ["Sports", "City", "Audio", "Crosswords"];
-
   return (
-    <div className="App">
-        <WelcomeHeader
-          title="HOMECOMING 2024" 
-          imageSrc={homecomingImage}
-        />
-        {/* <FirstFold>
-          <NavTabs />
-          <MainImage src="https://spectatorpublishing.github.io/nsopCampusMap/" frameBorder="0" scrolling="no" />
-          <MobileMainImage src="https://cds-static-hosting.s3.amazonaws.com/NSOP-ILLO.jpg" />
-          <a href="https://212west72.com/the-building?utm_source=ColumbiaSpectator&utm_medium=columbiaspectator&utm_campaign=Columbia%20Spectator%20Banners" target="_blank">
-            <Ad src="https://s0.2mdn.net/10483125/212W72_061521_300x600.gif"></Ad>
-        </a>
-        </FirstFold> */}
-        <a href="https://212west72.com/the-building?utm_source=ColumbiaSpectator&utm_medium=columbiaspectator&utm_campaign=Columbia%20Spectator%20Banners" target="_blank">
-            <MobileAd src="https://s0.2mdn.net/10483125/212W72_061521_300x600.gif"></MobileAd>
-        </a>
-        {sectionNames.map((sectionName, index) => (
-            <Section key={sectionName} sectionName={sectionName} index={index} />
-        ))}
-        <StaffCredits></StaffCredits>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+
+import Section from './components/Section';
+import StaffCredits from './components/StaffCredits';
+import WelcomeHeader from './components/WelcomeHeader';
+import homecomingImage from './homecoming.jpg';
+import { device } from './device';
+
+const FirstFold = styled.div`
+  display: flex;
+  margin-top: -3rem;
+  flex-direction: column-reverse;
+
+  @media ${device.tablet} {
+    flex-direction: row;
+    align-items: flex-start;
+  }
+`;
+
+const MainImage = styled.iframe`
+  display: none;
+
+  @media ${device.tablet} {
+    display: block;
+    width: 60%;
+    height: 35rem;
+  }
+`;
+
+const MobileMainImage = styled.img`
+  width: 100%;
+  height: 20rem;
+  display: block;
+
+  @media ${device.tablet} {
+    display: none;
+  }
+`;
+
+const Ad = styled.img`
+  margin: 0 2rem;
+  display: none;
+
+  @media ${device.tablet} {
+    display: block;
+  }
+`;
+
+const MobileAd = styled.img`
+  margin: 0 2rem;
+  display: block;
+
+  @media ${device.tablet} {
+    display: none;
+  }
+`;
+
+const Home = () => {
+  const sectionNames = ["Sports", "City", "Audio", "Crosswords"];
+
+  return (
+    <div>
+        <WelcomeHeader
+          title="HOMECOMING 2024" 
+          imageSrc={homecomingImage}
+        />
+        {sectionNames.map((sectionName, index) => (
+            <Section key={sectionName} sectionName={sectionName} index={index} />
+        ))}
+        <StaffCredits></StaffCredits>
+    </div>
+  );
+}
+
+export default Home;
