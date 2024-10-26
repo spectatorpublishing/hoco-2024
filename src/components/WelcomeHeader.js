771cc89110c80051f4eb642c5efc14a3012425bf
Darien Moment <dmoment18@gmail.com>
2024-10-26 15:21:39 -0400

    Heather name update

--- src/components/WelcomeHeader.js
+++ src/components/WelcomeHeader.js
@@ -73,7 +73,7 @@ export default function WelcomeHeader({ title, imageSrc  }) {
             
             <SignatureSection>
               <SignatureBlock>
-                <SignatureName>Heathen Chen</SignatureName>
+                <SignatureName>Heather Chen</SignatureName>
                 <SignatureTitle>Sports Editor</SignatureTitle>
               </SignatureBlock>
               <SignatureBlock>
